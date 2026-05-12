080cb44b1ba2db0e375e9ff365cef206ad5e8d79
Franco2912 <francoscout13@hotmail>
2026-05-12 19:29:06 -0300

    Inicio de main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log("UnaHur - Anti-Social net");
+const express = require('express');
+const { sequelize } = require('./db/models')
+const {} = require('./routers')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); // Para parsear el cuerpo de las solicitudes como JSON
+
+app.listen(PORT, async (err) => {
+    
+    if(err) {
+        console.log(err);
+        process.exit(1);
+    }
+    await sequelize.sync({});
+    console.log(`App inicia en el puerto ${PORT}`);
+});
