1da8358ec38bc7f68e0110a8826543f370a219be
Nicolas Diorio <nicolas.joaquin.diorio@gmail.com>
2026-05-17 18:51:14 -0300

    fix migraciones tags y post tags

--- src/db/migrations/20260513145213-create-tag.js
+++ src/db/migrations/20260513145213-create-tag.js
@@ -10,7 +10,9 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       nombre: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
       },
       createdAt: {
         allowNull: false,
@@ -21,8 +23,44 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
+
+    await queryInterface.createTable('PostTags', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      idPost: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Posts',
+          key: 'idPost',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      idTag: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Tags',
+          key: 'idTag',
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+    });
+
+    await queryInterface.addIndex('PostTags', ['idPost', 'idTag'], {
+      name: 'PostTags_idPost_idTag_unique',
+      unique: true,
+    });
   },
   async down(queryInterface, Sequelize) {
+    await queryInterface.removeIndex('PostTags', 'PostTags_idPost_idTag_unique');
+    await queryInterface.dropTable('PostTags');
     await queryInterface.dropTable('Tags');
   }
 };
