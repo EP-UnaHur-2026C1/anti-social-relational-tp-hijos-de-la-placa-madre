5ecc9cf068b09d921caef9eed89e344f86845627
Franco2912 <francoscout13@hotmail>
2026-05-20 20:57:31 -0300

    Se implemento middleware genericos y schema de user.

--- src/routers/router.user.js
+++ src/routers/router.user.js
@@ -1,14 +1,23 @@
 const {Router} = require('express')
-const {getAllUsers, getUserById, postUser, putUser, deleteUser} = require('../controllers/user.controller')
+
+const {getAllUsers, getUserById, postUser, putUser, deleteUser} = require('../controllers/user.controller') //SCRUD
+
+const { validateExistsModel, validarPathParameterMiddleware } = require('../middlewares/validateExists')// Middleware para validar que el recurso existe antes de ejecutar el controlador
+const { validateSchema } = require('../middlewares/validateSchema')// Middleware para validar el cuerpo de la solicitud con un esquema de Joi
+
+const { userCreateSchema } = require('../schemas/user.schema')// Esquema de validación para la creación de un usuario
+
+const { User } = require('../db/models') // Importamos el modelo de User para usarlo en el middleware de validación de existencia
+
 const router = Router()
 
 router.get('/usuarios', getAllUsers)
-router.get('/usuario/:id', getUserById)
+router.get('/usuario/:id', validarPathParameterMiddleware, validateExistsModel(User), getUserById)
 
-router.post('/usuario', postUser)
+router.post('/usuario', validateSchema(userCreateSchema), postUser)
 
-router.put('/usuario/:id', putUser)
+router.put('/usuario/:id', validateSchema(userCreateSchema),validarPathParameterMiddleware, validateExistsModel(User), putUser)
 
-router.delete('/usuario/:id', deleteUser)
+router.delete('/usuario/:id', validarPathParameterMiddleware, validateExistsModel(User), deleteUser)
 
 module.exports = router
